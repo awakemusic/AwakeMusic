de7c87660d67de8c91181adde0ec8202e6d1f705
zhubide <651861465@qq.com>
2019-06-17 16:16:09 +0800

    删除重复的东西

--- 完善功能/tcpserver/music.cpp
+++
@@ -1,4 +0,0 @@
-#include "music.h"
-
-Music::Music(QString name, QString singer, QString lyric, QString album, QString audiopath):m_name{name}, m_singer{singer}, m_lyric{lyric}, m_album{album}, m_audiopath{audiopath}
-{}
